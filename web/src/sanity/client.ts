83e7e8e989b3c622593451778e769c75b3e43924
Medhane34e <daniaregawi3@gmail.com>
2026-06-11 12:14:52 +0300

    Jun 11, 2026:- write token added

--- web/src/sanity/client.ts
+++ web/src/sanity/client.ts
@@ -3,6 +3,7 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  token: process.env.SANITY_API_WRITE_TOKEN,
   apiVersion: "2026-05-15",
   useCdn: false,
 });
